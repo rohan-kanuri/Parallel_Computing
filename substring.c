e04f7577e6480124a1b1d661be913e36c1399d38
rohan-kanuri <rohan.kanuri@gmail.com>
2017-04-20 16:39:38 +0530

    Create substring.c

---
+++ substring.c
@@ -0,0 +1,47 @@
+/*
+ * C Program To Count the Occurence of a Substring in String
+ */
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+char str[1000], sub[100];
+int count = 0, count1 = 0;
+
+void main()
+{
+    int i, j, l, l1, l2;
+
+    printf("\nEnter a string : ");
+    scanf("%[^\n]s", str);
+    l1 = strlen(str);
+
+    printf("\nEnter a substring : ");
+    scanf(" %[^\n]s", sub);
+
+    l2 = strlen(sub);
+clock_t t;
+    t = clock();
+    for (i = 0; i < l1;)
+    {
+        j = 0;
+        count = 0;
+        while ((str[i] == sub[j]))
+        {
+            count++;
+            i++;
+            j++;
+        }
+        if (count == l2)
+        {
+            count1++;
+            count = 0;
+        }
+        else
+            i++;
+    }
+t = clock() - t;
+    double time_taken = ((double)t)/CLOCKS_PER_SEC;
+printf("%.6f",time_taken);
+    printf("%s occurs %d times in %s", sub, count1, str);
+}
